2fa39bf364782bcdb08591084e86695fe224888a
Nazar <nazar.ignatenkoru@gmail.com>
2025-02-28 15:20:18 +0300

    menu completed

---
+++ BlazorAppNet8/Classes/CustomNavLink.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+namespace BlazorAppNet8.Classes
+{
+	public class CustomNavLink
+	{
+		public string NavText { get; set; } = "Home";
+		public string NavHref { get; set; } = "/";
+		public string NavIconName { get; set; } = @Icons.Material.Filled.Home;
+		public Color IconColor { get; set; } = Color.Primary;
+		public CustomNavLink? Children { get; set; } = null;
+	}
+}
